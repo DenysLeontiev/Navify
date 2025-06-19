b7ce93e01e80c199b6bdb9e557d068017547dde2
Denys Leon <leonden0046@gmail.com>
2025-06-19 12:22:39 +0300

    home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 import { LocationTrackerComponent } from './components/location-tracker/location-tracker.component';
 
 export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
     { path: 'location-tracker', component: LocationTrackerComponent },
 ];
