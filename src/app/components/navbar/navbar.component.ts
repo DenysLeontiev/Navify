c426e06b5c243ef06225a78be0274818c4e6ead3
Denys Leon <leonden0046@gmail.com>
2025-08-10 20:10:20 +0300

    Enhanced UI

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -6,17 +6,19 @@ import { LanguageService } from '../../_services/language/language.service';
 import { FormsModule } from '@angular/forms';
 import { Journey } from '../../models/journey';
 import { LocalStorageService } from '../../_services/localStorage/local-storage.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, TranslatePipe, FormsModule],
+  imports: [RouterLink, TranslatePipe, FormsModule, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
 
   public selectedLanguage: string = "";
+  public isMenuCollapsed = true;
 
   constructor(public translate: TranslateService, private languageService: LanguageService, private l: LocalStorageService) {
     this.languageService.setSelectedLanguageBasedOnBrowser();
